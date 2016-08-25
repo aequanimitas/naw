e42d701d786e81d6f359bb53f5aaa6ad81a61bf6
aequanimitas <htavenido@gmail.com>
2016-08-25 14:37:54 +0800

    testing abstraction

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-var knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host : '127.0.0.1',
-    user : 'naw_user',
-    password : 'naw_pass',
-    database : 'naw_db'
-  },
-  migrations: {
-    tableName: 'migrations'
-  }
-});
+//var knex = require('knex')({
+//  client: 'pg',
+//  connection: {
+//    host : '127.0.0.1',
+//    user : 'naw_user',
+//    password : 'naw_pass',
+//    database : 'naw_db'
+//  },
+//  migrations: {
+//    tableName: 'migrations'
+//  }
+//});
 
 var args = process.argv.slice(2);
 
-knex('naw_item').insert({naw_name: args[0], added: knex.raw('now()')}).then(function(e) {
-  console.log(e);  
-}, function(e) {
-  console.log(e);
-});
+var hasShortOption = function(x) {
+  return x.indexOf('-') === 1;
+};
+
+var hasLongOption = function(x) {
+  return x.lastIndexOf('--') === 0;
+};
+
+var options = function(fn) {
+  return args.filter(fn);
+}
+
+var shortOptions = options(hasShortOption);
+var longOptions = options(hasLongOption);
+
+console.log('shortOptions:', shortOptions);
+console.log('longOptions:', longOptions);
+
+//knex('naw_item').insert({naw_name: args[0], added: knex.raw('now()')}).then(function(e) {
+//  console.log(e);  
+//}, function(e) {
+//  console.log(e);
+//});
